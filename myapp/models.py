1e9811f2ea18c21aef091babee464fdecdd64e3b
SimonAloo <simonlopatin@gmail.com>
2023-08-12 14:52:42 +0400

    wrongdata

--- myapp/models.py
+++ myapp/models.py
@@ -1,12 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class User(models.Model):
-    email = models.EmailField(primary_key=True, unique=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     fam = models.CharField(max_length=255)
     name = models.CharField(max_length=255)
     otc = models.CharField(max_length=255)
     phone = models.CharField(max_length=255)
+
+    # Уникальное ограничение для поля email
+    email = models.EmailField(unique=True)
+
+    def __str__(self):
+        return self.user.email
 
 
 class Coords(models.Model):
